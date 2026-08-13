762ceb5d7ebe81466f43189b3425b3e376292798
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2026-08-13 19:40:41 +0300

    Update CatalogCard.tsx

--- frontend/src/components/CatalogCard.tsx
+++ frontend/src/components/CatalogCard.tsx
@@ -9,22 +9,22 @@ interface CatalogCardProps {
 export const CatalogCard: React.FC<CatalogCardProps> = ({ data }) => {
 
     return (
-        <div className="container-card">
+        <div className="container-card" data-testid="catalog-item">
             <div className='container-card-img'>
 <img src={data.imageUrl} alt={data.name} style={{ maxWidth: '100%', maxHeight: '100%' }} />
 
             </div>
             <div className='container-card-text'>
-                <div className='container-card-text-title'>{data.name}</div>
+                <div className='container-card-text-title' data-testid="catalog-item-name">{data.name}</div>
                 <div className='container-card-text-sub-title'>{data.description}</div>
                 <div className='container-card-text-bottom'>
-                    <div className='container-card-text-bottom-price'>{`${data.price.amount} ₽`}</div>
+                    <div className='container-card-text-bottom-price' data-testid="catalog-item-price">{`${data.price.amount} ₽`}</div>
                     {data.available ? 
-                    <div className='container-card-text-bottom-available'>В наличии</div>
-                     : <div className='container-card-text-bottom-not-available'>Отсутствует</div>
+                    <div className='container-card-text-bottom-available'data-testid="catalog-item-availability" data-availability='true'>В наличии</div>
+                     : <div className='container-card-text-bottom-not-available' data-availability='false'>Отсутствует</div>
                      }
                 </div>
-                <Link className='link-to-item' to={`/product/${data.slug}`} data-testid="catalog-item-name">К товару</Link>
+                <Link className='link-to-item' to={`/product/${data.slug}`}>К товару</Link>
             </div>
 
         </div>
