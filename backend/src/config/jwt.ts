5a0e505db5658b08702e0e9642cc646ba892b579
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2026-08-16 14:01:32 +0300

    Update jwt.ts

--- backend/src/config/jwt.ts
+++ backend/src/config/jwt.ts
@@ -1,5 +1,8 @@
 const rawSecret = process.env.JWT_SECRET;
-if (!rawSecret) {
+const isTest = process.env.NODE_ENV === 'test';
+
+if (!rawSecret && !isTest) {
   throw new Error('JWT_SECRET must be set in environment variables');
 }
-export const JWT_SECRET: string = rawSecret;
+
+export const JWT_SECRET: string = rawSecret || 'testsecret';
